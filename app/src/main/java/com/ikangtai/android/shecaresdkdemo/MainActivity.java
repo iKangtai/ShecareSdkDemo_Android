77373db705909093f6f94d0f9e3976743881adbd
zhusc <zhusc@ikangtai.com>
2018-03-22 20:31:39 +0800

    第四版

--- app/src/main/java/com/ikangtai/android/shecaresdkdemo/MainActivity.java
+++ app/src/main/java/com/ikangtai/android/shecaresdkdemo/MainActivity.java
@@ -1,18 +1,20 @@
 package com.ikangtai.android.shecaresdkdemo;
 
 import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.ikangtai.android.shecaresdk.OnDataListener;
 import com.ikangtai.android.shecaresdk.ShecareSdk;
-import com.ikangtai.android.shecaresdk.ble.OnBindListener;
-import com.ikangtai.android.shecaresdk.databean.ReceiveData;
+import com.ikangtai.android.shecaresdk.ble.bind.OnUnBindListener;
+import com.ikangtai.android.shecaresdk.ble.connect.OnReceiveDataListener;
 import com.ikangtai.android.shecaresdk.databean.net.req.UploadData;
 import com.ikangtai.android.shecaresdk.databean.net.resp.AnalysisResp;
 import com.ikangtai.android.shecaresdk.databean.net.resp.UploadDataResp;
@@ -29,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private String macAddress;
-    private String version;
+    private SharedPreferences sp;
+    private List<Temperature> temperatureList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
         Button btn9 = (Button) findViewById(R.id.btn_9);
         final Button btn10 = (Button) findViewById(R.id.btn_10);
         final TextView textView = (TextView) findViewById(R.id.tv_1);
+//
+        // 获取已经绑定过的设备的地址
+        sp = getSharedPreferences("yinglinyihao", Context.MODE_PRIVATE);
+        macAddress = sp.getString("address", null);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
                         UploadData.TempsBean tempsBean = new UploadData.TempsBean();
                         tempsBean.setTempTime("2018-03-01 12:12:12");
                         tempsBean.setTempValue("36.60");
-                        tempsBean.setDeleted(0);
                         uploadData.setTemps(temperatures);
                         // 添加经期
                         List<UploadData.PeriodsBean> periodsBeans = new ArrayList<>();
@@ -77,8 +83,8 @@ public class MainActivity extends AppCompatActivity {
                         periodsBeans.add(periodsBean1);
                         UploadData.PeriodsBean periodsBean2 = new UploadData.PeriodsBean();
                         periodsBean2.setPeriod(2);
-                        periodsBean2.setTime("2018-03-01");
-                        periodsBean2.setDeleted(0);
+                        periodsBean2.setTime("2018-03-06");
+                        periodsBean2.setDeleted(1);
                         periodsBeans.add(periodsBean2);
                         uploadData.setPeriods(periodsBeans);
                         //添加生理信息
@@ -119,9 +125,17 @@ public class MainActivity extends AppCompatActivity {
 
                 List<Temperature> list = new ArrayList<>();
                 Temperature temperature = new Temperature();
-                temperature.setTemp(34.6);
+                temperature.setTemp(36.66);
                 temperature.setDate("2018-02-22 12:12:12");
+                temperature.setDeleted(0);
+
+                Temperature temperature1 = new Temperature();
+                temperature1.setTemp(36.77);
+                temperature1.setDeleted(0);
+                temperature1.setDate("2018-02-23 12:12:12");
+
                 list.add(temperature);
+                list.add(temperature1);
 //                temperature.setTemp();
                 ShecareSdk.uploadTemps("AA:AA:AA:AA:AA:AA", list, new OnCallBack<UploadDataResp>
                         () {
@@ -145,13 +159,13 @@ public class MainActivity extends AppCompatActivity {
                 List<Period> periodsBeans = new ArrayList<>();
                 Period periodsBean1 = new Period();
                 periodsBean1.setPeriod(1); // 1表示经期开始，2表示经期结束
-                periodsBean1.setTime("2018-03-01");
-//                periodsBean1.setDeleted(0); //1表示删除，0表示未删除
+                periodsBean1.setTime("2018-02-11");
+                periodsBean1.setDeleted(0); //1表示删除，0表示未删除
                 periodsBeans.add(periodsBean1);
                 Period periodsBean2 = new Period();
                 periodsBean2.setPeriod(2);
-                periodsBean2.setTime("2018-03-01");
-//                periodsBean2.setDeleted(0);
+                periodsBean2.setTime("2018-02-15");
+                periodsBean2.setDeleted(0);
                 periodsBeans.add(periodsBean2);
 
                 ShecareSdk.uploadPeriods(periodsBeans, new OnCallBack<UploadDataResp>() {
@@ -202,9 +216,11 @@ public class MainActivity extends AppCompatActivity {
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ShecareSdk.unBindDevice(macAddress, new OnBindListener() {
+                ShecareSdk.unBindDevice(macAddress, new OnUnBindListener() {
                     @Override
                     public void unBindSuccess() {
+                        //解绑成功后，清楚sp存储的已经绑定的蓝牙地址
+                        sp.edit().putString("address", null);
                         Toast.makeText(ShecareSdk.getContext(), "解绑成功", Toast.LENGTH_LONG).show();
                     }
 
@@ -216,32 +232,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        temperatureList = new ArrayList<>();
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-                ShecareSdk.setOnDeviceDataListener(new OnDataListener() {
+                ShecareSdk.setOnDeviceDataListener(macAddress, new OnReceiveDataListener() {
                     @Override
-                    public void onBindState(int state, String msg, String macAddress, String version) {
-
-                    }
-
-                    @Override
-                    public void onDeviceData(final ReceiveData deviceData) {
+                    public void onReceiverData(List<Temperature> temps) {
+                        //因为体温计每次最多上传10条数据，所以如果体温计中历史数据多于十条，会分多次上传，所以此处需要注意
+                        temperatureList.addAll(temps);
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                List<Temperature> temperatures = deviceData.getTemperatures();
                                 StringBuffer sb = new StringBuffer();
-                                for (Temperature temperature : temperatures) {
+                                for (Temperature temperature : temperatureList) {
                                     if (temperature != null) {
                                         sb.append(temperature.getDate()).append("  ").append(temperature.getTemp());
                                     }
                                 }
-                                textView.setText(sb);
+                                textView.setText(sb.toString());
                             }
                         });
+                    }
+
+                    @Override
+                    public void onReceiverFailure(int code, String msg) {
+
                     }
                 });
             }
@@ -286,9 +303,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (resultCode == RESULT_OK) {
             macAddress = data.getStringExtra("macAddress");
-            version = data.getStringExtra("version");
-            Toast.makeText(MainActivity.this, macAddress + "  " + version, Toast.LENGTH_SHORT)
-                    .show();
+            if (!TextUtils.isEmpty(macAddress)) {
+                Toast.makeText(ShecareSdk.getContext(), macAddress, Toast.LENGTH_SHORT)
+                        .show();
+
+                //存储已经绑定的设备地址，这里存储在sp中，具体保存方式可根据你们的业务逻辑来处理
+                sp.edit().putString("address", macAddress).commit();
+            }
         }
 
     }
